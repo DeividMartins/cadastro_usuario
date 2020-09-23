9ff287a020199f2440180f7286b81312c0ecba06
Deivid Martins <deividmartins8902@gmail.com>
2020-09-22 22:13:54 -0300

    Comitando componente cadastro

---
+++ src/components/home/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Main from '../template/Main';
+
+export default props =>
+    <Main icon="home" title="Inicío"
+        subtitle="Segundo Projeto do capítulo de react.">
+        <div className="display-4">
+            Bem Vindo!
+            </div>
+        <hr />
+        <p className="mb-0">Sistema para exemplificar a construção de um desenvolvimento em React!</p>
+    </Main>
